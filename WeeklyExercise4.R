8f4d05178e61b6eaf1cd07f43f46ef056e631146
Gulshan <gmast3@uic.edu>
2026-09-23 09:10:33 -0500

    countries with largest athletes delegations

--- WeeklyExercise4.R
+++ WeeklyExercise4.R
@@ -5,3 +5,46 @@
 install.packages("usethis")
 library(usethis)
 git_default_branch()
+
+# Step 6. Olympics data analysis
+install.packages("tidyverse")
+library(readr)
+library(tidyverse)
+library(ggplot2)
+
+olympics <- read_csv("Olympics.csv")
+glimpse(olympics)
+
+# Creating total medals and adding it to the dataset
+olympics <- olympics %>%
+  mutate(total.medals = gold + silver + bronze)
+head(olympics)
+
+# Total gold medals won by each country
+gold_by_country <- olympics %>%
+  group_by(country) %>%
+  summarise(total_gold = sum(gold, na.rm = TRUE)) %>%
+  filter(total_gold > 0) %>%
+  arrange(desc(total_gold))
+print(gold_by_country, n = Inf)
+
+# Total medals given out each year
+medals_by_year <- olympics %>%
+  group_by(year) %>%
+  summarise(total.medals = sum(total.medals, na.rm = TRUE))
+print(medals_by_year, n = Inf)
+
+# Step 7
+library(readr)
+library(tidyverse)
+library(ggplot2)
+
+# a. Countries with largest athletes delegations in 1992
+delegations_1992 <- olympics %>%
+  filter(year == 1992) %>%
+  select( country, athletes) %>%
+  filter(athletes > 0) %>%
+  arrange(desc(athletes))
+delegations_1992
+
+
