509a7f3853ab964b290e8d3b27e19712423fe5fb
Gulshan <gmast3@uic.edu>
2026-09-23 09:31:37 -0500

    sorted merged

--- WeeklyExercise4.R
+++ WeeklyExercise4.R
@@ -6,10 +6,34 @@ install.packages("usethis")
 library(usethis)
 git_default_branch()
 
-# Step 7
 
 # Step 6. Olympics data analysis
 install.packages("tidyverse")
+
+olympics <- read_csv("Olympics.csv")
+glimpse(olympics)
+
+# Creating total medals and adding it to the dataset
+olympics <- olympics %>%
+  mutate(total.medals = gold + silver + bronze)
+head(olympics)
+
+# Total gold medals won by each country
+gold_by_country <- olympics %>%
+  group_by(country) %>%
+  summarise(total_gold = sum(gold, na.rm = TRUE)) %>%
+  filter(total_gold > 0) %>%
+  arrange(desc(total_gold))
+print(gold_by_country, n = Inf)
+
+# Total medals given out each year
+medals_by_year <- olympics %>%
+  group_by(year) %>%
+  summarise(total.medals = sum(total.medals, na.rm = TRUE))
+print(medals_by_year, n = Inf)
+
+
+# Step 7
 
 library(readr)
 library(tidyverse)
@@ -37,28 +61,6 @@ ggplot(gold_five, aes(x = year, y= gold, color = country)) +
        x = "Year",
        y = "Number of gold medals",
        color = "Country")
-
-olympics <- read_csv("Olympics.csv")
-glimpse(olympics)
-
-# Creating total medals and adding it to the dataset
-olympics <- olympics %>%
-  mutate(total.medals = gold + silver + bronze)
-head(olympics)
-
-# Total gold medals won by each country
-gold_by_country <- olympics %>%
-  group_by(country) %>%
-  summarise(total_gold = sum(gold, na.rm = TRUE)) %>%
-  filter(total_gold > 0) %>%
-  arrange(desc(total_gold))
-print(gold_by_country, n = Inf)
-
-# Total medals given out each year
-medals_by_year <- olympics %>%
-  group_by(year) %>%
-  summarise(total.medals = sum(total.medals, na.rm = TRUE))
-print(medals_by_year, n = Inf)
 
 
 
